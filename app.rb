83f5d6cb3b8b4d8c4c398de2344e039d988b5455
Osha Groetz <ogroetz@gmail.com>
2023-07-13 01:37:28 +0000

    form working

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  # "hello there"
+  erb(:hello)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/goodbye") do
+  erb(:goodbye)
 end
